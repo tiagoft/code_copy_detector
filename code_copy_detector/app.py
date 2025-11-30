0f6761295ce0c7c0388b35fbbcf5fe354a2ccae4
EduardoMVAz <emendesvaz@gmail.com>
2025-11-30 03:13:11 -0300

    fix: correctly importing new types

--- code_copy_detector/app.py
+++ code_copy_detector/app.py
@@ -2,26 +2,7 @@ import code_copy_detector as ccd
 from typing import Optional, List, Tuple, Dict
 import os
 from itertools import combinations
-
-import tokenize
-from dataclasses import dataclass
-
-# Type alias for a token list (List of TokenInfo objects)
-TokenList = List[tokenize.TokenInfo]
-
-# Type alias for an n-gram (Tuple of n TokenInfo objects)
-NGram = Tuple[tokenize.TokenInfo, ...]
-
-@dataclass
-class NGramData:
-    file: str
-    ngram: List[NGram]
-
-# Type alias for the token type tuple (keys in the dictionary)
-TokenTypeTuple = Tuple[int, ...]
-
-# Type alias for the final n-gram dictionary
-NGramDict = Dict[TokenTypeTuple, List[NGramData]]
+from code_copy_detector.code_copy_detector_types import TokenList, NGramDict
 
 class CodeCopyDetectorApp:
     @staticmethod
--- code_copy_detector/baselib.py
+++ code_copy_detector/baselib.py
@@ -1,23 +1,7 @@
 import tokenize
 from typing import Dict, List, Optional, Set, Tuple
-from dataclasses import dataclass
+from code_copy_detector.code_copy_detector_types import NGram, NGramData, TokenList, TokenTypeTuple, NGramDict
 
-# Type alias for a token list (List of TokenInfo objects)
-TokenList = List[tokenize.TokenInfo]
-
-# Type alias for an n-gram (Tuple of n TokenInfo objects)
-NGram = Tuple[tokenize.TokenInfo, ...]
-
-@dataclass
-class NGramData:
-    file: str
-    ngram: List[NGram]
-
-# Type alias for the token type tuple (keys in the dictionary)
-TokenTypeTuple = Tuple[int, ...]
-
-# Type alias for the final n-gram dictionary
-NGramDict = Dict[TokenTypeTuple, List[NGramData]]
 
 def get_token_list(fname: str, remove_imports: bool = True, remove_comments: bool = True) -> TokenList:
     with tokenize.open(fname) as f:
